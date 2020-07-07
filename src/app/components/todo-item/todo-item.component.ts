a71f5bf1ffa060674178302ae10b20767142a5f6
genius-softdev <webdevelopercrane@gmail.com>
2020-07-07 10:21:18 +0700

    Create TodoService

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,17 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Todo } from 'src/app/models/Todo';
+import { Component, OnInit, Input } from "@angular/core";
+import { Todo } from "src/app/models/Todo";
 
 @Component({
-  selector: 'app-todo-item',
-  templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.css']
+  selector: "app-todo-item",
+  templateUrl: "./todo-item.component.html",
+  styleUrls: ["./todo-item.component.css"],
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  // Set Dynamic Classes
+  setClasses() {
+    let classes = {
+      todo: true,
+      "is-complete": this.todo.completed,
+    };
+
+    return classes;
   }
 
+  onToggle(todo) {
+    todo.completed = !todo.completed;
+  }
+
+  onDelete(todo) {
+    console.log('delete');
+  }
 }
---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root",
+})
+export class TodoService {
+  constructor() {}
+
+  getTodos() {
+    return [
+      {
+        id: 1,
+        title: "Todo One",
+        completed: true,
+      },
+      {
+        id: 2,
+        title: "Todo Two",
+        completed: true,
+      },
+      {
+        id: 3,
+        title: "Todo Three",
+        completed: false,
+      },
+    ];
+  }
+}
